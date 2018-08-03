145cc9281d10f0382d1fe2a94ce9039038ab87d7
KCHAOU <kchaoucss@gmail.com>
2018-08-03 14:32:17 +0200

    Mock PersonServiceTest

--- src/test/java/com/kchaou/javalabs/PersonServiceTest.java
+++ src/test/java/com/kchaou/javalabs/PersonServiceTest.java
@@ -1,22 +1,74 @@
 package com.kchaou.javalabs;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.kchaou.javalabs.entities.Person;
 import com.kchaou.javalabs.repositories.IPersonRepository;
 import com.kchaou.javalabs.services.PersonServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class PersonServiceTest {
-	
+
 	@Mock
 	private IPersonRepository personRepo;
-	
+
 	@InjectMocks
 	private PersonServiceImpl personService;
-	
-	
 
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void get_all_persons() {
+
+		List<Person> personList = new ArrayList<Person>();
+
+		personList.add(new Person("bou7a", "bouhamed", "Courbevoie", "bou7a@gmail.com", "06631645848"));
+
+		when(personRepo.findAll()).thenReturn(personList);
+
+		List<Person> result = personService.getAllPerson();
+
+		assertEquals(1, result.size());
+	}
+
+	@Test
+	public void save_person() {
+
+		Person person = new Person("bou7a", "bouhamed", "Courbevoie", "bou7a@gmail.com", "06631645848");
+
+		when(personRepo.save(person)).thenReturn(person);
+
+		Person p = personService.savePerson(person);
+
+		assertEquals("bou7a", p.getName());
+		assertEquals("bouhamed", p.getFamilyName());
+	}
+
+	@Test
+	public void remove_person() {
+
+		Person person = new Person("bou7a", "bouhamed", "Courbevoie", "bou7a@gmail.com", "06631645848");
+
+		personService.removePerson(person);
+
+		verify(personRepo, times(1)).delete(person);
+
+	}
 }
